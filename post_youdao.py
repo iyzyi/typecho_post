2036ffa20e169898ed03e36f6f1510ab09481129
unknown <kljxn@qq.com>
2019-08-24 23:16:48 +0800

    Update post_youdao.py

--- post_youdao.py
+++ post_youdao.py
@@ -5,16 +5,11 @@ from urllib.parse import unquote
 def youdao(file_path):
     with open(file_path, 'r', encoding='utf-8') as f:
         txt = f.read()
-    '''
-    pic_iter = re.finditer(r'\!\[.*?\]\((file:///(.*?))\)', txt)
-    for tag in pic_iter:
-        pic_name = str(time.time()).replace('.','')+ '.png'
-        pic_url = post.upload_pic(tag.group(2), pic_name)
-        txt = txt.replace(tag.group(1), pic_url)
-    '''
     pic_iter = re.finditer(r'\!\[.*?\]\((.*?)\)', txt)
     for tag in pic_iter:
         pic_path = unquote(tag.group(1))    #图片名称含有汉字时，有道云笔记采用url编码的方式来记录，所以解码找到对应的图片位置
+        if re.search(r'^file:///(.+?)', pic_path):
+            pic_path = re.search(r'^file:///(.+)', pic_path).group(1)
         print(pic_path)
         pic_name = str(time.time()).replace('.','')+ '.png'
         pic_url = post.upload_pic(pic_path, pic_name)
@@ -25,11 +20,11 @@ def youdao(file_path):
 if __name__ == '__main__':
     post = Post()
     post.login()
-    category_name = 'CTF'
-    category_slug = 'ctf'
-    title = '第三次CTF双月赛'
+    category_name = 'WEB'
+    category_slug = 'web'
+    title = 'windows下使用phpstudy配置sqlilabs'
     category_mid = post.category(category_name, category_slug)
-    file_path = r'ctf3.md'
+    file_path = r'D:\桌面\44.md'
     text = youdao(file_path)
     if category_mid:
         post.send_text(title, text, category_mid)
